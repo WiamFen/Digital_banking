8cf6e0420c34b32147404f5c9ef6a28b06ea7478
WiamFen <wfenjiro@gmail.com>
2026-05-04 14:56:01 +0000

    mapping objet-relationnel avec JPA des entités Customer, BankAccount et AccountOperation

--- src/main/java/net/wiam/digital_banking/backend/entities/BankAccount.java
+++ src/main/java/net/wiam/digital_banking/backend/entities/BankAccount.java
@@ -1,5 +1,9 @@
 package net.wiam.digital_banking.backend.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,12 +12,16 @@ import net.wiam.digital_banking.backend.enums.AccountStatus;
 import java.util.Date;
 import java.util.List;
 
+@Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 public class BankAccount {
+    @Id
     private String id;
     private double balance;
     private Date createdAt;
     private AccountStatus status;
+    @ManyToOne
     private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
     private List<AccountOperation> accountOperations;
 }
