ccfa8607a136e7c260c2e5dc77661b59a55eeae6
WiamFen <wfenjiro@gmail.com>
2026-05-16 01:07:54 +0000

    gestion des comptes côté frontend Angular avec recherche, affichage des opérations bancaires, pagination et formulaires réactifs

--- frontend/src/app/accounts/accounts.ts
+++ frontend/src/app/accounts/accounts.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Account} from '../services/account';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {AccountDetails} from '../model/account.model';
+import {Observable} from 'rxjs';
+import {AsyncPipe, DatePipe, DecimalPipe, NgClass, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-accounts',
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf, AsyncPipe, DecimalPipe, NgForOf, DatePipe, NgClass],
   templateUrl: './accounts.html',
   styleUrl: './accounts.css',
   standalone: true,
 })
-export class Accounts {}
+export class Accounts implements OnInit{
+  accountFormGroup! : FormGroup;
+  currentPage : number = 0;
+  pageSize : number = 5;
+  accountObservable! : Observable<AccountDetails>
+
+  constructor(private fb: FormBuilder,private accountService : Account) { }
+  ngOnInit() {
+    this.accountFormGroup=this.fb.group({
+      accountId : this.fb.control('')
+    })
+  }
+
+  handleSearchAccount() {
+    let accountId : string=this.accountFormGroup.value.accountId;
+    this.accountObservable=this.accountService.getAccount(accountId,this.currentPage,this.pageSize);
+  }
+
+  gotoPage(page: number) {
+    this.currentPage=page;
+    this.handleSearchAccount();
+  }
+}
